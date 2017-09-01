56332405f2d5c44cb56465c500b5f16a87b50cda
khandakar Shadid <khandakarshadid@gmail.com>
2017-09-01 13:44:20 -0700

    updated location-list, index.js controller, locations.js
    Loop through using jade

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+
+//Require controller files
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
+//Define routes and map them to controller functions
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
